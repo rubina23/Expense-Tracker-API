8f2f041a7507012fccd6bd83ff5160a851b0341f
rubina <rubinacse14@gmail.com>
2026-08-23 15:51:54 +0600

    Updated to Transaction pooler

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # আপনার অনলাইন PostgreSQL URL এখানে দিন
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres.pvdlkwejindlnwxdvklx:[YOUR-PASSWORD]@aws-0-ap-southeast-2.pooler.supabase.com:6543/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.pvdlkwejindlnwxdvklx:etapi2026forex@aws-0-ap-southeast-2.pooler.supabase.com:6543/postgres"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@host/dbname"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
